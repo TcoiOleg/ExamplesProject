9e079199289f28f2c02fa6db74a4cf6026e7b4b6
1 <tsoyolv@gmail.com>
2015-07-17 23:05:06 +0300

    add 2003 task and some 2 examples for foreach with index

---
+++ src/main/java/ru/sgu/acm/solutions/task/taskimpls/LeapYearTaskImpl2004.java
@@ -0,0 +1,22 @@
+package ru.sgu.acm.solutions.task.taskimpls;
+
+import ru.sgu.acm.solutions.task.Task;
+
+import java.util.Scanner;
+
+/**
+ * User: 1
+ * Date: 17.07.15
+ */
+public class LeapYearTaskImpl2004 implements Task {
+    @Override
+    public String execute() {
+        int year = Integer.parseInt(inputData.get(0));
+        return String.valueOf(((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 1 : 0);
+    }
+
+    @Override
+    public void initInputData(Scanner scanner) {
+        inputData.add(scanner.nextLine());
+    }
+}
