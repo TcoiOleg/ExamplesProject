a64cbc39f4b19fc08a2e33822dcb910d9da3d587
Oleg <tsoyolv@gmail.com>
2016-04-21 00:42:06 +0400

    added ap1 mergeTwo from codingbat

--- src/main/java/ru/sgu/acm/solutions/Solution.java
+++ src/main/java/ru/sgu/acm/solutions/Solution.java
@@ -1,13 +1,22 @@
 package ru.sgu.acm.solutions;
 
 
+import ru.sgu.acm.solutions.task.codingbat.AP1;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 
+    public static final AP1 AP_1 = new AP1();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(Arrays.toString(AP_1.new MergeTwo().mergeTwo(new String[]{"a", "c", "z"}, new String[]{"a", "c", "z"}, 3)));
+        //exec(scanner);
+    }
+
+    private static void exec(Scanner scanner) {
         int n = scanner.nextInt();
         int mas[] = new int[n],
                 tempMas[],
---
+++ src/main/java/ru/sgu/acm/solutions/task/codingbat/AP1.java
@@ -0,0 +1,62 @@
+package ru.sgu.acm.solutions.task.codingbat;
+
+/**
+olts
+ */
+public class AP1 {
+
+/*Start with two arrays of strings, A and B, each with its elements in alphabetical order and without duplicates.
+Return a new array containing the first N elements from the two arrays. The result array should be in alphabetical
+order and without duplicates. A and B will both have a length which is N or more. The best "linear" solution makes
+a single pass over A and B, taking advantage of the fact that they are in alphabetical order, copying elements
+directly to the new array.
+mergeTwo(["a", "c", "z"], ["b", "f", "z"], 3) → ["a", "b", "c"]
+mergeTwo(["a", "c", "z"], ["c", "f", "z"], 3) → ["a", "c", "f"]
+mergeTwo(["f", "g", "z"], ["c", "f", "g"], 3) → ["c", "f", "g"]
+* */
+    public class MergeTwo {
+        public String[] mergeTwo(String[] a, String[] b, int n) {
+            if (n == 0) {
+                return new String[0];
+            }
+            String[] temp = merge(a, b, n);
+            return retrieveFirstNUniqueElements(n, temp);
+        }
+
+        private String[] merge(String[] a, String[] b, int n) {
+            String[] temp = new String[n + n];
+            for (int i = 0, k = 0; i < n; i++) {
+                if (a[i].compareTo(b[i]) < 0) {
+                    k = iterate(a[i], b, temp, k, i);
+                } else {
+                    k = iterate(b[i], a, temp, k, i);
+                }
+            }
+            return temp;
+        }
+
+        private int iterate(String anotherString, String[] b, String[] temp, int k, int i) {
+            if (k != 0 && temp[k - 1].compareTo(anotherString) > 0) {
+                String t = temp[k - 1];
+                temp[k - 1] = anotherString;
+                temp[k] = t;
+            } else {
+                temp[k] = anotherString;
+            }
+            k++;
+            temp[k++] = b[i];
+            return k;
+        }
+
+        private String[] retrieveFirstNUniqueElements(int n, String[] temp) {
+            String[] arr = new String[n];
+            for (int i = 0, k = 0; i < n; i++, k++) {
+                if (i != 0 && arr[i - 1].equals(temp[k])) {
+                    k++;
+                }
+                arr[i] = temp[k];
+            }
+            return arr;
+        }
+    }
+}
