be4273032594cdf99574058d6f601dab2c39291b
Oleg <tsoyolv@gmail.com>
2016-10-27 10:56:43 +0300

    added embeded database

--- src/main/java/ru/webexamples/web/rest/api/CreateUserServlet.java
+++ src/main/java/ru/webexamples/web/rest/api/CreateUserServlet.java
@@ -1,15 +1,30 @@
 package ru.webexamples.web.rest.api;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import ru.webexamples.web.WebContext;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 
+@WebServlet("/createUser")
 public class CreateUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DataSource dataSource = WebContext.getBean("dataSource");
 
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet("select name FROM users where id = 1");
+        if (!sqlRowSet.next()) {
+            resp.getOutputStream().print("there is no data in table.");
+            return;
+        }
+        resp.getOutputStream().print(sqlRowSet.getString("name"));
     }
 }
---
+++ src/main/resources/webapps/web/test-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES (1, 'fasdfasdf', 'mkyong@gmail.com');
+INSERT INTO users VALUES (2, 'aaaf', 'alex@yahoo.com');
+INSERT INTO users VALUES (3, 'afds', 'joel@gmail.com');
